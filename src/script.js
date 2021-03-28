3ea2fb47f639e8016fbc3212b0ba5f8c3cf2018e
momow1127 <80729938+momow1127@users.noreply.github.com>
2021-03-28 12:11:31 -0400

    Added the folder structure 9a07b8b

--- src/script.js
+++ src/script.js
@@ -33,6 +33,7 @@ function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
+  let precipitationElement = document.querySelector("#precipitation");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
@@ -43,6 +44,7 @@ function displayTemperature(response) {
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
+  precipitationElement.innerHTML = response.data.precipitation.value;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
